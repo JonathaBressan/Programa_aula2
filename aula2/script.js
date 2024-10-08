f40a54a392b814bb0e57ecdf03ab0dbf2aab6088
Jonatha Geske Bressan <132519815+JonathaBressan@users.noreply.github.com>
2024-10-08 20:39:01 -0300

    aula_2

--- aula2/script.js
+++ aula2/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
-    $.getJSON("./data.json", function(data) {
-        alert(data.name);
+    $("#button1").on("click", function(event){
+        
+        
+        $.getJSON("./data.json", function(data) {
+            $("#resultado1").append(data.name + ", ");
+            $("#resultado1").append(data.age + " anos");
+            for(let i in data.cars){
+                $("#resultado1").append(data.cars[i]);
+            }
+            $("#resultado1").append("<br>");
+            $("#limpar").append("");
+        });
     });
-    
+    $("#limpar").on("click", function(event){
+        $("div[id^=resultado]").empty();
+        
+    });
 });
