9637cce0f7d0ec86a78ab7f6d35fcfec621d1f4a
Jonatha Geske Bressan <132519815+JonathaBressan@users.noreply.github.com>
2024-10-09 19:55:10 -0300

    ppp

---
+++ aula3/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(e){
+    $.getJSON("https://randomuser.me/api/?results=10&nat=br", function(data) {
+        alert(data.results[0].gender);
+    });
+});
+    /*
+// comando do resultado1
+    $("#button1").on("click", function(event){
+        $.getJSON("./data.json", function(data) {
+            $("#resultado1").append(data.name + ", ");
+            $("#resultado1").append(data.age + " anos. ");
+            for(let i in data.cars){
+                $("#resultado1").append(data.cars[i] + " ");
+            }
+            $("#resultado1").append("<br>");
+            $("#limpar").append("");
+        });
+    });
+// comando do resultado2
+    $("#button2").on("click", function(event){
+        $.getJSON("./data2.json", function(data) {
+            $("#resultado2").append(data.name + ", ");
+            $("#resultado2").append(data.age + " anos. <br> ");
+            for(let i in data.friends){
+                let friendName = data.friends[i].firstName + " " + data.friends[i].lastName;
+                $("#resultado2").append("Amigo " + (parseInt(i)+1) + ": " + friendName + "<br>");
+            }
+            $("#resultado2").append("Total de amigos de " + data.name + ": " + data.friends.length + "<br>" );
+            $("#resultado2").append("<br>");
+            $("#limpar").append("");
+        });
+    });
+// comando do resultado3
+    $("#button3").on("click", function(event){
+        $.getJSON("./data3.json", function(data) {
+            for(let i in data.results){
+                $("#resultado3").append("Genêro: " + data.results[i].gender + "<br>");
+                $("#resultado3").append("Nome: " + data.results[i].name.first + "<br>");
+                $("#resultado3").append("Email: " + data.results[i].email + "<br>");
+            }
+            $("#resultado3").append("Info: " + data.info.seed + "<br>");
+        });
+    });
+
+//comando para limpar
+    $("#limpar").on("click", function(event){
+        $("div[id^=resultado]").empty();
+        
+    });
+});
+*/
